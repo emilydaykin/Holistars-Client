632cfd28ce0cb83350b5a60a1436ef9cddfa286b
Emily Daykin <emilydaykin31@gmail.com>
2022-04-25 14:37:12 +0200

    fixed search bug in cities

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { searchCities } from '../api/cities_api';
 import { Link } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { selectAllUsers, selectUserById } from '../features/users/usersSlice';
-// import { selectAllCities } from '../features/cities/citiesSlice';
-// import { filterCities } from '../features/cities/citiesSlice';
+import { useSelector } from 'react-redux';
+import { selectAllCities } from '../features/cities/citiesSlice';
 import { getAllCities } from '../api/cities_api';
 import Stars from './Stars';
 
 const Cities = () => {
-  // const allCities = useSelector(selectAllCities);
+  const allCitiesRedux = useSelector(selectAllCities);
   const [cities, setCities] = useState(null);
-  const [searchInput, setSearchInput] = useState('');
 
-  // const users = useSelector((state) => selectUserById(state, Number(3)));
-  // const users = useSelector(selectAllUsers);
-  // console.log('USERS', users);
-
-  // console.log('CITIES', cities);
-
-  const getCityAvgRating = city =>
-    city.reviews.reduce((total, review) => total + review.avg_rating, 0) /
-    city.reviews.length;
+  const getCityAvgRating = (city) =>
+    city.reviews.reduce((total, review) => total + review.avg_rating, 0) / city.reviews.length;
 
   useEffect(() => {
     const getCityData = async () => {
@@ -38,23 +28,22 @@ const Cities = () => {
     'North America': 'DarkSlateGray',
     Africa: 'MidnightBlue',
     'Australia & Pacific': 'FireBrick',
-    'South America': 'SaddleBrown',
+    'South America': 'SaddleBrown'
   };
 
-  const filterThroughCities = async () => {
-    const filteredCities = await searchCities(searchInput);
-    // const filteredCities = filterCities(searchInput);
-    setCities(filteredCities);
+  const filterThroughCities = async (searchInput) => {
+    console.log('searchInput', searchInput);
+    if (searchInput) {
+      const filteredCities = await searchCities(searchInput);
+      setCities(filteredCities);
+    } else {
+      setCities(allCitiesRedux);
+    }
   };
 
-  const handleSearchChange = e => {
-    console.log('search', e.target.value);
-    setSearchInput(e.target.value);
-    filterThroughCities();
-    // filterCities(e.target.value);
+  const handleSearchChange = (e) => {
+    filterThroughCities(e.target.value);
   };
-
-  console.log(searchInput);
 
   return (
     <section className='cities'>
@@ -64,46 +53,37 @@ const Cities = () => {
         type='text'
         placeholder='Search city, country, continent, or recommended attractions...'
         onChange={handleSearchChange}
-        value={searchInput}
       ></input>
       <div className='cities__container'>
         {!cities ? (
           <p>Loading cities...</p>
         ) : (
-          cities.map(city => (
-            <Link
-              className='cities__city-card-link'
-              to={`/destinations/${city.id}`}
-              key={city.id}
-            >
+          cities.map((city) => (
+            <Link className='cities__city-card-link' to={`/destinations/${city.id}`} key={city.id}>
               <div className='card cities__city-card'>
                 <div
                   className='cities__city-image'
                   style={{
                     backgroundImage: `url(${city.image})`,
-                    backgroundSize: 'cover',
+                    backgroundSize: 'cover'
                   }}
                 ></div>
                 <div className='cities__city-text'>
                   <p className='cities__city-name'>{city.city}</p>
-                  <p>
+                  <div>
                     <span className='cities__country-name'>{city.country}</span>
                     ,&ensp;
                     <span
                       style={{
                         color: continentColorCodes[city.continent],
                         fontWeight: 300,
-                        fontSize: '1.25rem',
+                        fontSize: '1.25rem'
                       }}
                     >
                       {city.continent}
                     </span>
-                    {city.reviews.length > 0 ? (
-                      <Stars value={getCityAvgRating(city)} />
-                    ) : (
-                      ''
-                    )}
-                  </p>
+                    {city.reviews?.length > 0 ? <Stars value={getCityAvgRating(city)} /> : ''}
+                  </div>
                 </div>
               </div>
             </Link>
