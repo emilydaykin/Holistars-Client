c19c1b6d7441136a5b8a6dfa9397718809b499b6
Marco <mar.manunta@gmail.com>
2022-05-19 08:44:23 +0200

    x

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -8,21 +8,26 @@ const initialState = [];
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
   try {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}/authentication`);
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/authentication`
+    );
     return response.data;
   } catch (err) {
     return err.message;
   }
 });
 
-export const registerUser = createAsyncThunk('users/registerUser', async (newUser) => {
-  try {
-    const response = await axios.post(`${USERS_URL}/register/`, newUser);
-    return response.data;
-  } catch (err) {
-    return err.message;
+export const registerUser = createAsyncThunk(
+  'users/registerUser',
+  async newUser => {
+    try {
+      const response = await axios.post(`${USERS_URL}/register/`, newUser);
+      return response.data;
+    } catch (err) {
+      return err.message;
+    }
   }
-});
+);
 
 const usersSlice = createSlice({
   name: 'users',
@@ -36,11 +41,12 @@ const usersSlice = createSlice({
       .addCase(registerUser.fulfilled, (state, action) => {
         state.users.push(action.payload);
       });
-  }
+  },
 });
 
-export const selectAllUsers = (state) => state.users;
-export const selectUserById = (state, id) => state.users.find((user) => user.id === id);
+export const selectAllUsers = state => state.users;
+export const selectUserById = (state, id) =>
+  state.users.find(user => user.id === id);
 
 export const { userRegistered } = usersSlice.actions;
 
